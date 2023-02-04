5d19f47b500236ea5d1b48b2e24b7fe6c43bb89c
Lyna-NA <67642540+Lyna-NA@users.noreply.github.com>
2023-02-04 13:43:10 +0200

    Firebase Chat App: Search User

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,56 @@
+import { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase";
+
 let Search = () => {
-    return(
-        <div className="search">
-            <div className="searchForm">
-                <input type="text" name="" id="" placeholder="Find a user"/>
-            </div>
-        </div>
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const searchHandler = async () => {
+    // Create a query against the collection "users"
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
     );
-}
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(doc.id, " => ", doc.data());
+      });
+    } catch (error) {
+      setErr(true);
+    }
+  };
+
+  const handelKey = (event) => {
+    event.code === "Enter" && searchHandler();
+  };
+
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Find a user"
+          onKeyDown={handelKey}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      {err && <span>User not found!</span>}
+      {user && <div className="userChat">
+        <img
+          src={user.photoURL}
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>{user.displayName}</span>
+        </div>
+      </div>}
+    </div>
+  );
+};
 export default Search;
